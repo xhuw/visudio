9e176417247124a08e8f9885d49d7283f9ef2229
Huw Percival <huwpercival@xmos.com>
2023-08-09 10:40:59 +0100

    User can select device

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
-use cpal::traits::{HostTrait, DeviceTrait};
-use cpal::ALL_HOSTS;
-fn main() {
+use std::io::Write;
 
+use cpal::{
+    traits::{DeviceTrait, HostTrait},
+    Device,
+};
+
+fn get_index_from_user(max: usize) -> std::io::Result<usize> {
+    let mut user_input = String::new();
+    loop {
+        print!("Select microphone by index (0 - {}): ", max);
+        std::io::stdout().flush()?;
+
+        std::io::stdin().read_line(&mut user_input)?;
+        if let Ok(index) = user_input.trim().parse::<usize>() {
+            if index < max {
+                return Ok(index);
+            }
+        }
+        user_input.clear();
+    }
+}
+
+fn run_app(device: &Device) {}
+
+fn main() -> Result<(), &'static str> {
     let host = cpal::default_host();
-    println!("{:?}", host.id());
 
     if let Ok(devices) = host.input_devices() {
-        for d in devices {
-            println!("{:?}", d.name());
+        let devices: Vec<_> = devices.collect();
+        let indices = (0..).map(|i| i.to_string());
+        let device_names = devices.iter().map(|d| d.name().unwrap_or("".to_string()));
+        for (i, name) in std::iter::zip(indices, device_names) {
+            println!("{i} - {name}")
         }
-    }
-    println!("{:?}", ALL_HOSTS);
 
-    println!("Hello, world!");
+        println!("");
+        let selected =
+            get_index_from_user(devices.len() - 1).map_err(|_| "Failed to read from stdin")?;
+        run_app(&devices[selected]);
+    } else {
+        return Err("This computer has no supported audio hosts");
+    }
+
+    Ok(())
 }
