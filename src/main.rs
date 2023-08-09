5c0f40ea993f05d1bb0d496d096d0853a672a668
Huw Percival <huwpercival@xmos.com>
2023-08-09 16:56:22 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -33,76 +33,66 @@ fn build_stream(device: &Device, config: SupportedStreamConfig) -> Result<Stream
         eprintln!("an error occurred on stream: {}", err);
     };
     let d = match config.sample_format() {
-        SampleFormat::I8 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[i8], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::I16 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[i16], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::I32 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[i32], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::I64 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[i64], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::U8 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[u8], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::U16 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[u16], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::U32 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[u32], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::U64 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[u64], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::F32 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[f32], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
-        SampleFormat::F64 => device
-            .build_input_stream(
-                &config.into(),
-                |data: &[f64], _: &_| dump(data[0]),
-                err_fn,
-                None,
-            )?,
+        SampleFormat::I8 => device.build_input_stream(
+            &config.into(),
+            |data: &[i8], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::I16 => device.build_input_stream(
+            &config.into(),
+            |data: &[i16], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::I32 => device.build_input_stream(
+            &config.into(),
+            |data: &[i32], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::I64 => device.build_input_stream(
+            &config.into(),
+            |data: &[i64], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::U8 => device.build_input_stream(
+            &config.into(),
+            |data: &[u8], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::U16 => device.build_input_stream(
+            &config.into(),
+            |data: &[u16], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::U32 => device.build_input_stream(
+            &config.into(),
+            |data: &[u32], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::U64 => device.build_input_stream(
+            &config.into(),
+            |data: &[u64], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::F32 => device.build_input_stream(
+            &config.into(),
+            |data: &[f32], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
+        SampleFormat::F64 => device.build_input_stream(
+            &config.into(),
+            |data: &[f64], _: &_| dump(data[0]),
+            err_fn,
+            None,
+        )?,
         _ => return Err(anyhow!("Unsupported format")),
     };
     Ok(d)
@@ -110,29 +100,32 @@ fn build_stream(device: &Device, config: SupportedStreamConfig) -> Result<Stream
 
 fn run_app(device: &Device) -> Result<(), anyhow::Error> {
     // TODO select sample rate
-    let config = device
-        .default_input_config()?;
+    let config = device.default_input_config()?;
     println!("using default sample rate: {}", config.sample_rate().0);
     let stream = build_stream(device, config)?;
     let _ = stream.play().unwrap();
     loop {}
 }
 
-fn main() -> Result<(), anyhow::Error>{
+fn main() -> Result<(), anyhow::Error> {
     let host = cpal::default_host();
 
-    let devices = host.input_devices()? ;
-    let device_names: Vec<_> = devices.map(|d| d.name().unwrap_or("".to_string())).collect();
-    for (i, name) in  device_names.iter().enumerate() {
-         
+    let devices = host.input_devices()?;
+    let device_names: Vec<_> = devices
+        .map(|d| d.name().unwrap_or("".to_string()))
+        .collect();
+    for (i, name) in device_names.iter().enumerate() {
         println!("{i} - {name}")
     }
 
     println!("");
-    let selected =
-        get_index_from_user(device_names.len() - 1)?;
-    
-    if let Some(device) = host.input_devices()?.find(|d| d.name().map(|y| y == device_names[selected]).unwrap_or(false)) {
+    let selected = get_index_from_user(device_names.len() - 1)?;
+
+    if let Some(device) = host.input_devices()?.find(|d| {
+        d.name()
+            .map(|y| y == device_names[selected])
+            .unwrap_or(false)
+    }) {
         run_app(&device)?;
     }
 
